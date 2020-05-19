2612bff29ecc46db8ed9339fea0bd419131d5f98
Mohit Gupta <mohit.gupta@service.nsw.gov.au>
2020-05-19 19:11:14 +1000

    commit my work

---
+++ Arrays1.js
@@ -0,0 +1,22 @@
+let student1 = "Christine"
+let student2 = "Dave"
+let student3 = "Haley"
+let students = ["Christine","Dave","Haley"];
+console.log(student1)
+console.log(students[2])
+
+console.log("This is our first list of students: ")
+console.log(students)
+
+console.log("This is our new list after adding a student")
+
+students.push("Trent");
+
+console.log(students)
+students.pop();
+console.log(students)
+
+students.pop();
+console.log(students)
+students.pop();
+console.log(students)
